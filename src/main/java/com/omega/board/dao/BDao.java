58d67a19a6e62f60d2601ae6ea3e92d63d10727f
Younggil Lee <leyoungg@sheridancollege.ca>
2017-05-04 18:24:59 -0400

    set up list view page, write view page, and write page

--- src/main/java/com/omega/board/dao/BDao.java
+++ src/main/java/com/omega/board/dao/BDao.java
@@ -1,6 +1,7 @@
 package com.omega.board.dao;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,16 +19,18 @@ public class BDao {
 	
 	DataSource dataSource;
 	
+	
 	public BDao(){
 		
-		try {
+	/*	try {
 			Context context = new InitialContext();
 			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/Oracle11g");
 			
 		} catch (NamingException e) {
 			
 			e.printStackTrace();
-		}
+		}*/	
+		
 	}
 	
 	public ArrayList<BDto> list(){
@@ -36,9 +39,13 @@ public class BDao {
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String uid = "scott";
+		String upw = "1234";
 		
 		try{
-			connection = dataSource.getConnection();
+			//connection = dataSource.getConnection();
+			connection = DriverManager.getConnection(url,uid,upw);
 			String query  = "select bId, bName, bTitle, bContent, bDate, bHit, bGrouop, bStep, bIndent"
 					+ "from mvc_board order by bGroup desc, bSteop asc";
 			preparedStatement = connection.prepareStatement(query);
@@ -79,4 +86,46 @@ public class BDao {
 		return dtos;
 		
 	}
+	
+	public void write(String bName, String bTitle, String bContent){
+		
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		String url = "jdbc:oracle:thin:@localhost:1521:xe";
+		String uid = "scott";
+		String upw = "1234";
+		
+		try {
+			//connection = dataSource.getConnection();
+			connection = DriverManager.getConnection(url,uid,upw);
+			String query  = "insert into mvc_board(bId, bName, bTitle, bContent, bHit, bGroup, bStep, bIndent)"
+					+ "values(mvc_board_seq.nextval, ?, ?, ?, 0, mvc_board_seq.currval, 0, 0)";
+			preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, bName);
+			preparedStatement.setString(2, bTitle);
+			preparedStatement.setString(3, bContent);
+			
+			int result = preparedStatement.executeUpdate();
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+			
+		}finally{
+			
+			try{	
+				
+				if(preparedStatement != null) preparedStatement.close();
+				if(connection != null) connection.close();
+				
+				}catch(Exception e2){
+				
+					e2.printStackTrace();
+				
+				}
+			}
+		
+	}
+	
+	
 }
