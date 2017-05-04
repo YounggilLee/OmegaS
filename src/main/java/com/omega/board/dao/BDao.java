4f06889f9747d979881932324ea5bdc6f4fa36e8
Younggil Lee <leyoungg@sheridancollege.ca>
2017-05-04 17:16:22 -0400

    set up connection in BDao for getting data from database. input the data to BDto and store it to arraylist

--- src/main/java/com/omega/board/dao/BDao.java
+++ src/main/java/com/omega/board/dao/BDao.java
@@ -1,5 +1,82 @@
 package com.omega.board.dao;
 
-public class BDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import com.omega.board.dto.BDto;
+
+public class BDao {
+	
+	DataSource dataSource;
+	
+	public BDao(){
+		
+		try {
+			Context context = new InitialContext();
+			dataSource = (DataSource) context.lookup("java:comp/env/jdbc/Oracle11g");
+			
+		} catch (NamingException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<BDto> list(){
+		
+		ArrayList<BDto> dtos = new ArrayList<BDto>();
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		ResultSet resultSet = null;
+		
+		try{
+			connection = dataSource.getConnection();
+			String query  = "select bId, bName, bTitle, bContent, bDate, bHit, bGrouop, bStep, bIndent"
+					+ "from mvc_board order by bGroup desc, bSteop asc";
+			preparedStatement = connection.prepareStatement(query);
+			
+			while(resultSet.next()){
+				int bId = resultSet.getInt("bId");
+				String bName = resultSet.getString("bName");
+				String bTitle = resultSet.getString("bTitle");
+				String bContent = resultSet.getString("bContent");
+				Timestamp bDate = resultSet.getTimestamp("bDate");
+				int bHit = resultSet.getInt("bHit");
+				int bGroup = resultSet.getInt("bGroup");
+				int bStep = resultSet.getInt("bStep");
+				int bIndent = resultSet.getInt("bIndent");
+				
+				BDto dto = new BDto(bId, bName, bTitle, bContent, bDate, bHit, bGroup, bStep, bIndent);
+				dtos.add(dto);
+						
+			}
+		}catch(Exception e){
+			
+			e.printStackTrace();
+		}finally{
+			
+				try {
+					
+					if(resultSet != null) resultSet.close();
+					if(preparedStatement != null) preparedStatement.close();
+					if(connection != null) connection.close();
+					
+				} catch (SQLException e2) {
+				
+					e2.printStackTrace();
+				}
+		}
+		
+		
+		return dtos;
+		
+	}
 }
